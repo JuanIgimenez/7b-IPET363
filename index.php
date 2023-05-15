2c139d8fc2ea83a3e336e4a8e3ba8cb8fbd2cf66
ElElys <132698328+ElElys@users.noreply.github.com>
2023-05-15 14:31:59 -0300

    el index y el css broders
    juan te quieropmucho bolo

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <div class="jider">
+            <p>cuenta lol</p>
+            <h1>TURISMO HERMOSO</h1>
+            <P>MENU</P>
+        </div>
+        <nav>
+            insertar barra de busqueda
+        </nav>
+    </header>
+    <div class="row">
+        <p class="col-md-6">centro</p>
+        <p class="col-md-6">Naty</p>
+    </div>
+    
+            <p class="col-md-6">Interes</p>
+            <p class="col-md-6">history</p>
+    
+</body>
+</html>
